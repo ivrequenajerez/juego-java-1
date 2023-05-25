30e5d3956e3c285d106acc854e762a1a0ec90118
ivrequenajerez <ianrequenajerez@gmail.com>
2023-05-25 15:50:24 +0200

    -

--- src/Interfaz/rondaDePreguntas.java
+++ src/Interfaz/rondaDePreguntas.java
@@ -4,6 +4,7 @@
  */
 package Interfaz;
 
+import java.util.ArrayList;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import trivia.Pregunta;
@@ -27,9 +28,8 @@ public class rondaDePreguntas extends javax.swing.JFrame {
         // Asignar el icono a la ventana principal
         setIconImage(icono.getImage());
         
-        //Aqui insertas pregunta
-        Pregunta pregunta = new Pregunta("\u00bfQu\u00e9 videojuegos son de estrategia?", respuestas);
-        
+        // Crear pregunta
+        CrearPregunta();
     }
 
     /**
@@ -54,10 +54,7 @@ public class rondaDePreguntas extends javax.swing.JFrame {
         jButton10 = new javax.swing.JButton();
         jButton11 = new javax.swing.JButton();
         jButton12 = new javax.swing.JButton();
-        jButton13 = new javax.swing.JButton();
-        jButton14 = new javax.swing.JButton();
-        jButton15 = new javax.swing.JButton();
-        jButton16 = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
         fondoPreg = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
@@ -74,67 +71,57 @@ public class rondaDePreguntas extends javax.swing.JFrame {
                 jButton1ActionPerformed(evt);
             }
         });
-        fondoResp.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 30, 130, 60));
+        fondoResp.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 50, 130, 60));
 
         jButton2.setBackground(new java.awt.Color(255, 255, 255));
         jButton2.setText("OPCION");
-        fondoResp.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 30, 130, 60));
+        fondoResp.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 50, 130, 60));
 
         jButton3.setBackground(new java.awt.Color(255, 255, 255));
         jButton3.setText("OPCION");
-        fondoResp.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 30, 130, 60));
+        fondoResp.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 50, 130, 60));
 
         jButton4.setBackground(new java.awt.Color(255, 255, 255));
         jButton4.setText("OPCION");
-        fondoResp.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 30, 130, 60));
+        fondoResp.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 50, 130, 60));
 
         jButton5.setBackground(new java.awt.Color(255, 255, 255));
         jButton5.setText("OPCION");
-        fondoResp.add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 100, 130, 60));
+        fondoResp.add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 140, 130, 60));
 
         jButton6.setBackground(new java.awt.Color(255, 255, 255));
         jButton6.setText("OPCION");
-        fondoResp.add(jButton6, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 100, 130, 60));
+        fondoResp.add(jButton6, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 140, 130, 60));
 
         jButton7.setBackground(new java.awt.Color(255, 255, 255));
         jButton7.setText("OPCION");
-        fondoResp.add(jButton7, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 100, 130, 60));
+        fondoResp.add(jButton7, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 140, 130, 60));
 
         jButton8.setBackground(new java.awt.Color(255, 255, 255));
         jButton8.setText("OPCION");
-        fondoResp.add(jButton8, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 100, 130, 60));
+        fondoResp.add(jButton8, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 140, 130, 60));
 
         jButton9.setBackground(new java.awt.Color(255, 255, 255));
         jButton9.setText("OPCION");
-        fondoResp.add(jButton9, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 170, 130, 60));
+        fondoResp.add(jButton9, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 230, 130, 60));
 
         jButton10.setBackground(new java.awt.Color(255, 255, 255));
         jButton10.setText("OPCION");
-        fondoResp.add(jButton10, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 170, 130, 60));
+        fondoResp.add(jButton10, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 230, 130, 60));
 
         jButton11.setBackground(new java.awt.Color(255, 255, 255));
         jButton11.setText("OPCION");
-        fondoResp.add(jButton11, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 170, 130, 60));
+        fondoResp.add(jButton11, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 230, 130, 60));
 
         jButton12.setBackground(new java.awt.Color(255, 255, 255));
         jButton12.setText("OPCION");
-        fondoResp.add(jButton12, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 170, 130, 60));
+        fondoResp.add(jButton12, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 230, 130, 60));
 
-        jButton13.setBackground(new java.awt.Color(255, 255, 255));
-        jButton13.setText("OPCION");
-        fondoResp.add(jButton13, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 240, 130, 60));
-
-        jButton14.setBackground(new java.awt.Color(255, 255, 255));
-        jButton14.setText("OPCION");
-        fondoResp.add(jButton14, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 240, 130, 60));
-
-        jButton15.setBackground(new java.awt.Color(255, 255, 255));
-        jButton15.setText("OPCION");
-        fondoResp.add(jButton15, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 240, 130, 60));
-
-        jButton16.setBackground(new java.awt.Color(255, 255, 255));
-        jButton16.setText("OPCION");
-        fondoResp.add(jButton16, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 240, 130, 60));
+        jLabel3.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel3.setText("Ronda 1");
+        fondoResp.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 10, -1, -1));
 
         fondoPreg.setBackground(new java.awt.Color(51, 51, 51));
 
@@ -152,8 +139,8 @@ public class rondaDePreguntas extends javax.swing.JFrame {
         fondoPregLayout.setHorizontalGroup(
             fondoPregLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(fondoPregLayout.createSequentialGroup()
-                .addGap(211, 211, 211)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 158, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(30, 30, 30)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 429, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(33, 33, 33))
@@ -189,7 +176,36 @@ public class rondaDePreguntas extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
-
+    
+    private void CrearPregunta() { // como parametro puede ponerse la ronda
+        // Crear pregunta de ronda 1
+        ArrayList<String> preguntas = new ArrayList<>();
+        ArrayList<String> respuestas1 = new ArrayList<>();
+        ArrayList<String> respuestas2 = new ArrayList<>();
+        ArrayList<String> respuestas3 = new ArrayList<>();
+        ArrayList<String> respuestas4 = new ArrayList<>();
+        Pregunta pregunta = new Pregunta(preguntas, respuestas1);
+        pregunta.AñadeDatos();
+        preguntas = pregunta.getPreguntas();
+        respuestas1 = pregunta.getRespuestas1();
+        respuestas2 = pregunta.getRespuestas2();
+        respuestas3 = pregunta.getRespuestas3();
+        respuestas4 = pregunta.getRespuestas4();
+        jLabel1.setText(preguntas.get(0)); // Establece la primera pregunta en un JLabel
+        jButton1.setText(respuestas1.get(1)); // Establece la primera respuesta de la primera pregunta en un JButton
+        jButton2.setText(respuestas1.get(2)); // Establece la segunda respuesta de la primera pregunta en un JButton
+        jButton3.setText(respuestas1.get(3)); // Establece la segunda respuesta de la primera pregunta en un JButton
+        jButton4.setText(respuestas1.get(4)); // Establece la segunda respuesta de la primera pregunta en un JButton
+        jButton5.setText(respuestas1.get(5)); // Establece la segunda respuesta de la primera pregunta en un JButton
+        jButton6.setText(respuestas1.get(6)); // Establece la segunda respuesta de la primera pregunta en un JButton
+        jButton7.setText(respuestas1.get(7)); // Establece la segunda respuesta de la primera pregunta en un JButton
+        jButton8.setText(respuestas1.get(8)); // Establece la segunda respuesta de la primera pregunta en un JButton
+        jButton9.setText(respuestas1.get(9)); // Establece la segunda respuesta de la primera pregunta en un JButton
+        jButton10.setText(respuestas1.get(10)); // Establece la segunda respuesta de la primera pregunta en un JButton
+        jButton11.setText(respuestas1.get(11)); // Establece la segunda respuesta de la primera pregunta en un JButton
+        jButton12.setText(respuestas1.get(12)); // Establece la segunda respuesta de la primera pregunta en un JButton
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -232,10 +248,6 @@ public class rondaDePreguntas extends javax.swing.JFrame {
     private javax.swing.JButton jButton10;
     private javax.swing.JButton jButton11;
     private javax.swing.JButton jButton12;
-    private javax.swing.JButton jButton13;
-    private javax.swing.JButton jButton14;
-    private javax.swing.JButton jButton15;
-    private javax.swing.JButton jButton16;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
@@ -246,5 +258,6 @@ public class rondaDePreguntas extends javax.swing.JFrame {
     private javax.swing.JButton jButton9;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     // End of variables declaration//GEN-END:variables
 }
--- src/trivia/Pregunta.java
+++ src/trivia/Pregunta.java
@@ -15,16 +15,24 @@ public class Pregunta {
 
     // Atributos
     // 12 preguntas y 12 respuestas. Listas
-    private ArrayList preguntas = new ArrayList(12);
-    private ArrayList respuestas1 = new ArrayList(12);
-    private ArrayList respuestas2 = new ArrayList(12);
-    private ArrayList respuestas3 = new ArrayList(12);
-    private ArrayList respuestas4 = new ArrayList(12);
+    private ArrayList<String> preguntas = new ArrayList<>();
+    private ArrayList<String> respuestas1 = new ArrayList<>();
+    private ArrayList<String> respuestas2 = new ArrayList<>();
+    private ArrayList<String> respuestas3 = new ArrayList<>();
+    private ArrayList<String> respuestas4 = new ArrayList<>();
     
     // Constructores
+
+    public Pregunta() {
+    }
     
     public Pregunta(ArrayList preguntas, ArrayList respuestas) {
-    
+        
+        String pregunta1 = "¿Qué videojuegos son de estrategia?";
+        String pregunta2 = "¿Quiénes son cantantes andaluces?";
+        String pregunta3 = "¿Cuáles son raperos de España?";
+        String pregunta4 = "¿Cuáles son pueblos de Andalucía?";
+        
         respuestas1.add("Age of Empires");
         respuestas1.add("Civilization");
         respuestas1.add("Total War");
@@ -38,7 +46,7 @@ public class Pregunta {
         respuestas1.add("The Sims");
         respuestas1.add("Valorant");
         // Pregunta 1
-        preguntas.add(new Pregunta("¿Qué videojuegos son de estrategia?", respuestas1));
+        preguntas.add(pregunta1);
         
         respuestas2.add("Alejandro Sanz");
         respuestas2.add("Rosalía");
@@ -53,38 +61,151 @@ public class Pregunta {
         respuestas2.add("Ed Sheeran");
         respuestas2.add("Adele");
         // Pregunta 2
-        preguntas.add(new Pregunta("¿Quiénes son cantantes andaluces?", respuestas2));
+        preguntas.add(pregunta2);
         
-        respuestas2.add("Kase.O");
-        respuestas2.add("Arianna Puello");
-        respuestas2.add("Ayax");
-        respuestas2.add("YuS");
-        respuestas2.add("Mala Rodríguez");
-        respuestas2.add("FERNANDOACOSTA");
-        respuestas2.add("Canserbero");
-        respuestas2.add("Apache");
-        respuestas2.add("Lil Supa");
-        respuestas2.add("MC Teteu");
-        respuestas2.add("Fazzini");
-        respuestas2.add("Hurricane G");
+        respuestas3.add("Kase.O");
+        respuestas3.add("Arianna Puello");
+        respuestas3.add("Ayax");
+        respuestas3.add("YuS");
+        respuestas3.add("Mala Rodríguez");
+        respuestas3.add("FERNANDOACOSTA");
+        respuestas3.add("Canserbero");
+        respuestas3.add("Apache");
+        respuestas3.add("Lil Supa");
+        respuestas3.add("MC Teteu");
+        respuestas3.add("Fazzini");
+        respuestas3.add("Hurricane G");
         // Pregunta 3
-        preguntas.add(new Pregunta("¿Cuáles son raperos de España?", respuestas3));
+        preguntas.add(pregunta3);
         
-        respuestas2.add("Albacete");
-        respuestas2.add("Venta de Pantalones");
-        respuestas2.add("Buenas Noches");
-        respuestas2.add("Salares");
-        respuestas2.add("Matagorda");
-        respuestas2.add("Guarromán");
-        respuestas2.add("Cenicero");
-        respuestas2.add("Correpoco");
-        respuestas2.add("Elciego");
-        respuestas2.add("Eulz");
-        respuestas2.add("Moveros");
-        respuestas2.add("Pis");
+        respuestas4.add("Albacete");
+        respuestas4.add("Venta de Pantalones");
+        respuestas4.add("Buenas Noches");
+        respuestas4.add("Salares");
+        respuestas4.add("Matagorda");
+        respuestas4.add("Guarromán");
+        respuestas4.add("Cenicero");
+        respuestas4.add("Correpoco");
+        respuestas4.add("Elciego");
+        respuestas4.add("Eulz");
+        respuestas4.add("Moveros");
+        respuestas4.add("Pis");
         // Pregunta 4
-        preguntas.add(new Pregunta("¿Cuáles son pueblos de Andalucía?", respuestas4));
-        
+        preguntas.add(pregunta4);
     } 
+    
+    // Métodos
+    
+    public void AñadeDatos() {
+        String pregunta1 = "¿Qué videojuegos son de estrategia?";
+        String pregunta2 = "¿Quiénes son cantantes andaluces?";
+        String pregunta3 = "¿Cuáles son raperos de España?";
+        String pregunta4 = "¿Cuáles son pueblos de Andalucía?";
+        
+        respuestas1.add("Age of Empires");
+        respuestas1.add("Civilization");
+        respuestas1.add("Total War");
+        respuestas1.add("StarCraft");
+        respuestas1.add("Company of Heroes");
+        respuestas1.add("Call of Duty");
+        respuestas1.add("FIFA");
+        respuestas1.add("Minecraft");
+        respuestas1.add("Grand Theft Auto");
+        respuestas1.add("Super Mario Bros.");
+        respuestas1.add("The Sims");
+        respuestas1.add("Valorant");
+        // Pregunta 1
+        preguntas.add(pregunta1);
+        
+        respuestas2.add("Alejandro Sanz");
+        respuestas2.add("Rosalía");
+        respuestas2.add("Pablo Alborán");
+        respuestas2.add("India Martínez");
+        respuestas2.add("Niña Pastori");
+        respuestas2.add("David Bisbal");
+        respuestas2.add("Maluma");
+        respuestas2.add("Justin Bieber");
+        respuestas2.add("Churumbé");
+        respuestas2.add("Beyoncé");
+        respuestas2.add("Ed Sheeran");
+        respuestas2.add("Adele");
+        // Pregunta 2
+        preguntas.add(pregunta2);
+        
+        respuestas3.add("Kase.O");
+        respuestas3.add("Arianna Puello");
+        respuestas3.add("Ayax");
+        respuestas3.add("YuS");
+        respuestas3.add("Mala Rodríguez");
+        respuestas3.add("FERNANDOACOSTA");
+        respuestas3.add("Canserbero");
+        respuestas3.add("Apache");
+        respuestas3.add("Lil Supa");
+        respuestas3.add("MC Teteu");
+        respuestas3.add("Fazzini");
+        respuestas3.add("Hurricane G");
+        // Pregunta 3
+        preguntas.add(pregunta3);
+        
+        respuestas4.add("Albacete");
+        respuestas4.add("Venta de Pantalones");
+        respuestas4.add("Buenas Noches");
+        respuestas4.add("Salares");
+        respuestas4.add("Matagorda");
+        respuestas4.add("Guarromán");
+        respuestas4.add("Cenicero");
+        respuestas4.add("Correpoco");
+        respuestas4.add("Elciego");
+        respuestas4.add("Eulz");
+        respuestas4.add("Moveros");
+        respuestas4.add("Pis");
+        // Pregunta 4
+        preguntas.add(pregunta4);
+    }
+
+    public ArrayList<String> getPreguntas() {
+        return preguntas;
+    }
+
+    public void setPreguntas(ArrayList<String> preguntas) {
+        this.preguntas = preguntas;
+    }
+
+    public ArrayList<String> getRespuestas1() {
+        return respuestas1;
+    }
+
+    public void setRespuestas1(ArrayList<String> respuestas1) {
+        this.respuestas1 = respuestas1;
+    }
+
+    public ArrayList<String> getRespuestas2() {
+        return respuestas2;
+    }
+
+    public void setRespuestas2(ArrayList<String> respuestas2) {
+        this.respuestas2 = respuestas2;
+    }
+
+    public ArrayList<String> getRespuestas3() {
+        return respuestas3;
+    }
+
+    public void setRespuestas3(ArrayList<String> respuestas3) {
+        this.respuestas3 = respuestas3;
+    }
+
+    public ArrayList<String> getRespuestas4() {
+        return respuestas4;
+    }
+
+    public void setRespuestas4(ArrayList<String> respuestas4) {
+        this.respuestas4 = respuestas4;
+    }
+
+    @Override
+    public String toString() {
+        return "Pregunta{" + "preguntas=" + preguntas + ", respuestas1=" + respuestas1 + ", respuestas2=" + respuestas2 + ", respuestas3=" + respuestas3 + ", respuestas4=" + respuestas4 + '}';
+    }
     
 }
